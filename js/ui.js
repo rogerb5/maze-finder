47afbdd367aa823a0768ea969cd9c7727248169c
rogerb5 <rbenjume@ewu.edu>
2024-04-13 12:06:21 -0700

    add completed dark mode

--- js/ui.js
+++ js/ui.js
@@ -10,8 +10,10 @@ const buttons = document.querySelectorAll("button#action");
 const bfsButton = document.querySelector('button.bfs-button');
 const aboutButton = document.querySelector('button.about');
 const aboutSection = document.querySelector('section.about');
+const aboutArticles = document.querySelectorAll('article#dark-mode');
 const closeButton = document.querySelector('button.close');
 const darkModeButton = document.querySelector('button.DarkMode');
+const modalTitle = document.querySelector('h1.modal-title');
 
 // use other graphs found in graphs.txt to create different sizes of grid
 const defaultGrid = [
@@ -55,10 +57,26 @@ closeButton.addEventListener("click", () => {
 aboutButton.addEventListener("click", () => {
     aboutSection.classList.add('active');
 });
+
+
 darkModeButton.addEventListener("click", () => {
+    const dataOutput = document.querySelector('p.p-data');
     document.body.classList.toggle('DarkMode');
-    console.log('Dark Mode Toggled');
+    dataOutput.classList.toggle('dark-mode');
+    aboutSection.classList.toggle('dark-mode');
+    closeButton.classList.toggle('dark-mode');
+    modalTitle.classList.toggle('dark-mode');
+
+    aboutArticles.forEach(article => {
+        article.classList.toggle('dark-mode');
+    })
+
+    buttons.forEach(btn => {
+        btn.classList.toggle('dark-button');
+    })
+
 });
+
 
 bfsButton.addEventListener("click", () => {
     if (graph.hasSource) {
